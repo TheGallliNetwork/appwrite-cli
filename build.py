b51cf21f6f60db7b2b90176b8247c85fce3b9b73
Bhagavan Nagaraju <bhagavan@jigijigi.in>
2023-12-18 12:39:46 +0530

    1.0.0

--- build.py
+++ build.py
@@ -13,7 +13,7 @@ summary = "An alternative CLI wrapper on top of Appwrite API with support " \
           "for creating & restoring snapshots to easily reproduce dev. " \
           "environments."
 default_task = "publish"
-version = "0.0.2"
+version = "1.0.0"
 author = "The Gallli Network"
 author_email = "support@gallistats.com"
 license = "MIT"
